9b5e3e45a183b55a1a59aa2535ecc2aad9fa71b7
rise <quinnyan@rdchina.net>
2024-12-04 10:05:14 +0800

    youtube 短视频下载

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
+  },
+  plugins: [],
+}
